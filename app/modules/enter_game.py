05281bf3a210713b84572bb067e93e4290341604
laozhu <1536075646@qq.com>
2024-09-24 20:21:12 +0800

    修改适配按钮时产生的判断错误

--- app/modules/enter_game.py
+++ app/modules/enter_game.py
@@ -7,28 +7,28 @@ class enter_game_module:
     def __init__(self):
         self.start_game = "app/resource/images/start_game/"
         self.in_game = "app/resource/images/in_game/"
+        self.start_button_root = False
 
     def enter_game(self):
-        start_button_root = None
         if is_exist_image(self.start_game + 'start.png'):
             print("已检测到黄色 开始游戏 按钮")
-            start_button_root = self.start_game + 'start.png'
+            self.start_button_root = self.start_game + 'start.png'
+
         elif is_exist_image(self.start_game + 'start_blue.png'):
             print("已检测到蓝色 开始游戏 按钮")
-            start_button_root = self.start_game + 'start_blue.png'
+            self.start_button_root = self.start_game + 'start_blue.png'
         else:
             print("未检测到开始游戏按钮,跳过自动登录")
             return
-
         if not is_exist_image(self.start_game + "already_in_game.png"):
-            if wait_for_image(self.start_game + "update_game.png", time_out=5) or wait_for_image(
-                    self.start_game + "start.png", time_out=5):
+            if wait_for_image(self.start_game + "update_game.png", time_out=5) or self.start_button_root:
                 if exist_then_click(self.start_game + "update_game.png"):
                     print("需要更新")
                     wait_then_click(self.start_game + "update_yes.png")
                     while is_exist_image(self.start_game + "updating.png"):
                         time.sleep(1)
-                wait_then_click(start_button_root, 5, 0.7)
+                print(self.start_button_root)
+                wait_then_click(self.start_button_root, 5, 0.7)
                 if wait_for_image(self.start_game + "age.png"):
                     wait_then_click(self.start_game + "start_2.png", 5)
                 # 首次登录与非首次登录进入主界面
