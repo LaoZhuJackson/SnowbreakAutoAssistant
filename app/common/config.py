3e3479d7eeb134b4f5997d796bb004cb865ae2dd
vmoranv <1761886648@qq.com>
2025-07-16 12:13:16 +0800

    ✨ feat:添加precommit检查,更新gitignore,删除nuitka打包错误报告,删除无用的语言适配类

--- app/common/config.py
+++ app/common/config.py
@@ -9,25 +9,6 @@ from qfluentwidgets import (qconfig, QConfig, ConfigItem, OptionsConfigItem, Boo
 from .setting import CONFIG_FILE
 
 
-class Language(Enum):
-    """ Language enumeration """
-
-    CHINESE_SIMPLIFIED = QLocale(QLocale.Chinese, QLocale.China)
-    CHINESE_TRADITIONAL = QLocale(QLocale.Chinese, QLocale.HongKong)
-    ENGLISH = QLocale(QLocale.English)
-    AUTO = QLocale()
-
-
-class LanguageSerializer(ConfigSerializer):
-    """ Language serializer """
-
-    def serialize(self, language):
-        return language.value.name() if language != Language.AUTO else "Auto"
-
-    def deserialize(self, value: str):
-        return Language(QLocale(value)) if value != "Auto" else Language.AUTO
-
-
 def isWin11():
     return sys.platform == 'win32' and sys.getwindowsversion().build >= 22000
 
